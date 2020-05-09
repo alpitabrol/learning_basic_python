535867155e13e6de16215e4110bf24c4056b8ac8
Alpit Abrol <apt.abl@gmail.com>
2020-05-08 23:35:38 -0400

    learned manuplation of files

--- Exercise Files/Ch4/ospathutils_start.py
+++ Exercise Files/Ch4/ospathutils_start.py
@@ -10,20 +10,26 @@ import time
 
 def main():
   # Print the name of the OS
-
+  # print(os.name)
 
   # Check for item existence and type
-
+  # print("item exists: "+str(path.exists("textfile.txt")))
+  # print("item is a file: "+str(path.isfile("textfile.txt")))
+  # print("item is a directory: "+str(path.isdir("textfile.txt")))
   
   # Work with file paths
-
+  # print("item path: "+str(path.realpath("textfile.txt")))
+  # print("item path and name: "+str(path.split(path.realpath("textfile.txt"))))
   
   # Get the modification time
+  # t=time.ctime(path.getmtime("textfile.txt"))
+  # print(t)
+  # print(datetime.datetime.fromtimestamp(path.getmtime("textfile.txt")))
 
-  
   # Calculate how long ago the item was modified
 
-
+  td=datetime.datetime.now() - datetime.datetime.fromtimestamp(path.getmtime("textfile.txt"))
+  print("it has been: "+str(td)+"since the file was modified")
   
 if __name__ == "__main__":
   main()
--- Exercise Files/Ch4/shell_start.py
+++ Exercise Files/Ch4/shell_start.py
@@ -4,28 +4,33 @@
 import os
 from os import path
 import shutil
-
-
+from shutil import make_archive
+from tarfile import TarFile
 def main():
   # make a duplicate of an existing file
   if path.exists("textfile.txt"):
-    # get the path to the file in the current directory
 
+    # get the path to the file in the current directory
+    src=path.realpath("textfile.txt")
     
     # let's make a backup copy by appending "bak" to the name
-
+    # dst = src+".bak"
     
     # copy over the permissions, modification times, and other info
-
+    #shutil.copy(src,dst)
 
     # rename the original file
-
+    # os.rename("textfile.txt","newfile.txt")
     
     # now put things into a ZIP archive
-
-
+    
+    # root_dir,tail = path.split(src)
+    # shutil.make_archive("archive","tar",root_dir)
+    
     # more fine-grained control over ZIP files
-
+    with TarFile("testtar.tar","w") as newtar:
+      newtar.add("textfile.txt")
+      newtar.add("textfile.txt.bak")
       
 if __name__ == "__main__":
   main()

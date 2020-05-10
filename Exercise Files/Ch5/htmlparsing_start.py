c2bddd4a8559f367668703cf3dd4b500bec0a758
Alpit Abrol <apt.abl@gmail.com>
2020-05-09 22:53:17 -0400

    learned about html editing and jason value calling using python

--- Exercise Files/Ch5/htmlparsing_start.py
+++ Exercise Files/Ch5/htmlparsing_start.py
@@ -1,13 +1,21 @@
 # 
 # Example file for parsing and processing HTML
 #
+from html.parser import HTMLParser
+
+class MyHTMLParser(HTMLParser):
+  def handle_starttag(self, tag,attrs):
+
+    print(tag)
+
 
 
 def main():
   # instantiate the parser and feed it some HTML
   parser = MyHTMLParser()
-    
-
+  f= open("samplehtml.html")
+  parser.feed(f.read())
+  
 if __name__ == "__main__":
   main();
   
